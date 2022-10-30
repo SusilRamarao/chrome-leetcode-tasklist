e96c374d7ec96e36a56367984fc4eb8327a7a353
Susil Ramarao <susilramarao@Susils-MacBook-Air.local>
2022-10-30 14:55:29 +0530

    progress

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,101 @@
+
+document.body.style.border = "5px solid red";
+
+var arr = [], l = document.links;
+
+for(var i=0; i<l.length; i++) {
+  arr.push(l[i].href);
+}
+
+for(var i = 0; i < arr.length; i++){
+
+    const temp = arr[i];
+    const wordMatchRegExp = /leetcode.com/g; 
+    const word = temp.match(wordMatchRegExp);
+    if(word){
+        var array = arr[i].split('/');
+        if(!array[array.length - 1]){
+
+            var lastsegment = array[array.length-2];
+        }
+        else{
+
+            var lastsegment = array[array.length-1];
+        }
+        
+        console.debug(lastsegment);
+    }
+    
+}
+
+
+var leetcodeURL = "https://leetcode.com/api/problems/algorithms/";
+/*
+var temp;
+fetch(leetcodeURL)
+//.then(res => res.json())
+.then(out =>
+  console.debug('Checkout this JSON! ', out))
+.catch(err => { console.debug(err) });
+
+console.debug
+*/
+/*
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.mode = "no-cors";
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    console.debug("Send")
+    xhr.send();
+};
+
+getJSON(leetcodeURL,
+function(err, data) {
+  if (err !== null) {
+    console.debug('Something went wrong: ' + err);
+  } else {
+    console.debug('Your query count: ' + data.query.count);
+  }
+});
+
+*/
+
+
+let data = {
+    query: "\n query userProblemsSolved($username: String!) {\n allQuestionsCount {\n difficulty\n count\n } \n matchedUser(username: $username) {\n problemsSolvedBeatsStats {\n difficulty\n percentage\n }\n subm itStatsGlobal {\n acSubmissionNum {\n difficulty\n count\n }\n }\n }\n}\n ",
+    variables: {
+    username: susil95,
+    }
+}
+
+fetch(leetcodeURL, {
+    method: "POST",
+    headers: {'Content-Type': 'application/json', 'mode':'no-cors'}, 
+    body: JSON.stringify(data)
+  }).then(res => {
+    console.debug("Request complete! response:", res.json());
+    }).then(out =>
+        console.debug('Checkout this JSON! ', out));
+
+
+
+/*
+var xhr = new XMLHttpRequest();
+xhr.open("POST", leetcodeURL, true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify(data));
+*/
+
+/*
 const article = document.querySelector("article");
 
 // `document.querySelector` may return null if the selector doesn't match anything.
@@ -20,3 +118,4 @@ if (article) {
 
   (date ?? heading).insertAdjacentElement("afterend", badge);
 }
+*/
